26cff6a17337f049a1b861912bdaf67500a03606
Ryan Bahl <ryanbahl9@gmail.com>
2022-05-23 20:12:02 -0700

    init push
    Co-authored-by: Edison Chen <edisonpchen@users.noreply.github.com>
    Co-authored-by: samcheng271 <samcheng271@users.noreply.github.com>

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -1,4 +1,10 @@
 from pacai.util import reflection
+# from pacai.agents.capture.offense import OffensiveReflexAgent
+# from pacai.agents.capture.defense import DefensiveReflexAgent
+from pacai.agents.capture.reflex import ReflexCaptureAgent
+from pacai.core.directions import Directions
+from pacai.student.multiagents import ReflexAgent
+
 
 def createTeam(firstIndex, secondIndex, isRed,
         first = 'pacai.agents.capture.dummy.DummyAgent',
@@ -10,10 +16,101 @@ def createTeam(firstIndex, secondIndex, isRed,
     and will be False if the blue team is being created.
     """
 
-    firstAgent = reflection.qualifiedImport(first)
-    secondAgent = reflection.qualifiedImport(second)
+    firstAgent = OffensiveAgent
+    secondAgent = DefensiveAgent
 
     return [
         firstAgent(firstIndex),
         secondAgent(secondIndex),
     ]
+
+class OffensiveAgent(ReflexCaptureAgent):
+    def __init__(self, index, **kwargs):
+        super().__init__(index)
+
+    def getFeatures(self, gameState, action):
+        features = {}
+        successor = self.getSuccessor(gameState, action)
+        features['successorScore'] = self.getScore(successor)
+
+        # Compute distance to the nearest food.
+        foodList = self.getFood(successor).asList()
+
+        # This should always be True, but better safe than sorry.
+        if (len(foodList) > 0):
+            myPos = successor.getAgentState(self.index).getPosition()
+            minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+            features['distanceToFood'] = minDistance
+
+        return features
+
+    def getWeights(self, gameState, action):
+        return {
+            'successorScore': 100,
+            'distanceToFood': -1
+        }
+    
+    def getAction(self, gameState):
+        return Directions.STOP
+        return super().getAction(gameState)
+
+
+class DefensiveAgent(ReflexCaptureAgent):
+    def __init__(self, index, **kwargs):
+        super().__init__(index)
+
+    def getFeatures(self, gameState, action):
+        features = {}
+
+        successor = self.getSuccessor(gameState, action)
+        myState = successor.getAgentState(self.index)
+        myPos = myState.getPosition()
+
+        # Computes whether we're on defense (1) or offense (0).
+        features['onDefense'] = 1
+        if (myState.isPacman()):
+            features['onDefense'] = 0
+
+        # Computes distance to invaders we can see.
+        enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
+        invaders = [a for a in enemies if a.isPacman() and a.getPosition() is not None]
+        features['numInvaders'] = len(invaders)
+        
+        
+        if (len(invaders) > 0):
+            dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
+            features['invaderDistance'] = min(dists)
+
+        if (action == Directions.STOP):
+            features['stop'] = 1
+
+        rev = Directions.REVERSE[gameState.getAgentState(self.index).getDirection()]
+        if (action == rev):
+            features['reverse'] = 1
+        
+        if (len(invaders) > 0):
+            foodList = self.getFoodYouAreDefending(successor).asList()
+
+            # This should always be True, but better safe than sorry.
+            if (len(foodList) > 0):
+                myPos = successor.getAgentState(self.index).getPosition()
+                minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+                features['distanceToFood'] = minDistance
+            features['invaderFoodDistance']
+
+        return features
+
+    def getWeights(self, gameState, action):
+        return {
+            'numInvaders': -1000,
+            'onDefense': 100,
+            'invaderDistance': -10,
+            'stop': -100,
+            'reverse': -2,
+            'invaderFoodDistance': -1
+        }
+    def getAction(self, gameState):
+        return Directions.STOP
+        return super().getAction(gameState)
+
+    
