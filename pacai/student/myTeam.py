3c69bcc1202734d620a56fc4580f668b4af4f72b
Ryan Bahl <ryanbahl9@gmail.com>
2022-05-30 19:26:56 -0700

    updated my team

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -1,3 +1,4 @@
+import random
 from pacai.core import distance
 from pacai.agents.capture.capture import CaptureAgent
 from pacai.util import reflection
@@ -287,19 +288,23 @@ class QLearningAgent(CaptureAgent):
         # Compute distance to the nearest food.
         foodList = self.getFood(successor).asList()
 
-        # Computer distance to nearest capsule
+        # Compute distance to nearest capsule
         capsuleList = self.getCapsules(successor)
         oldCapsuleList = self.getCapsules(gameState)
 
         # Get own position 
         myPos = successor.getAgentState(self.index).getPosition()
 
-        # Add capsule as ghost
+        # Add capsule as food
         foodList.extend(capsuleList)
-        
-        # reward PacMan for eating a capsule 
-        if myPos in oldCapsuleList:
-            features['eatCapsule'] = 1
+
+        # reward PacMan for getting close to and eating a capsule
+        if oldCapsuleList:
+            minDist = min([self.getMazeDistance(myPos, capsule) for capsule in oldCapsuleList])
+            features['eatCapsule'] = minDist
+
+        if len(capsuleList) < len(oldCapsuleList):
+            features['ateCapsule'] = 10
 
         # This should always be True, but better safe than sorry.
         if (len(foodList) > 0):
@@ -348,7 +353,7 @@ class QLearningAgent(CaptureAgent):
             # if ghosts are scared then go towards them
             # THIS IS NOT WORKING PACMAN STILL RUNS AWAY FROM SCARED GHOSTS and doesnt eat capsules
             if closestGhost.isScared():
-                distanceToEnemy *= -.1
+                distanceToEnemy *= -.0
 
             features['distanceToEnemy'] = distanceToEnemy
 
@@ -407,13 +412,13 @@ class QLearningAgent(CaptureAgent):
         if feature in self.weights:
             return self.weights.get(feature)
         initialWeights = {
-            'numInvaders': -1000,
-            'onDefense': 100,
-            'invaderDistance': -10,
-            'stop': -100,
-            'reverse': -2,
-            'invaderFoodDistance': 0,
-            'waitMiddle': -100
+            'successorScore': 100,
+            'distanceToFood': -1,
+            'distanceToEnemy': 100,
+            'stop': -200,
+            'distanceToCapsule': 0,
+            'eatCapsule': -2,
+            'ateCapsule': 100
         }
         return initialWeights.get(feature, 1)
 
