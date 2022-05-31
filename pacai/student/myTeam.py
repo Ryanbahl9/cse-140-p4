bb8a0eee5e38f90e55e8ed57577117bce4ee34e7
Ryan Bahl <ryanbahl9@gmail.com>
2022-05-30 19:22:12 -0700

    added qlearning

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -6,6 +6,8 @@ from pacai.util import reflection
 from pacai.agents.capture.reflex import ReflexCaptureAgent
 from pacai.core.directions import Directions
 from pacai.student.multiagents import ReflexAgent
+from pacai.util import probability
+
 
 
 def createTeam(firstIndex, secondIndex, isRed,
@@ -212,4 +214,207 @@ class GeneralAgent(CaptureAgent):
 
     def chooseAction(self, gameState):  
         return None  
+
+class QLearningAgent(CaptureAgent):
+    """
+        List of Class values:
+            self.index              : Index of current agent
+            self.red                : Whether or not you're on the red team
+            self.agentsOnTeam       : Agent objects controlling you and your teammates
+            self.distancer          : Maze distance calculator
+            self.observationHistory : A history of observations
+            self.timeForComputing   : Time to spend each turn on computing maze distances
+
+        Notes:
+            This probably could not be used for two agents 
+    """
+    def __init__(self, index, alpha = 1.0, epsilon = 0.05,
+            gamma = 0.8, numTraining = 10, **kwargs):
+        """
+        Args:
+            alpha: The learning rate.
+            epsilon: The exploration rate.
+            gamma: The discount factor.
+            numTraining: The number of training episodes.
+        """
+        super().__init__(index, **kwargs)
+
+        self.lastAction = None
+        self.alpha = float(alpha)
+        self.epsilon = float(epsilon)
+        self.discountRate = float(gamma)
+        self.numTraining = int(numTraining)
+        self.weights = {}
+
+    def chooseAction(self, gameState):
+        
+        self.update(gameState)
+
+        if probability.flipCoin(self.epsilon):
+            action = random.choice(gameState.getLegalActions(self.index))
+        else:
+            action = self.getPolicy(gameState)
+
+        self.lastAction = action
+        return action
+
+    def getPolicy(self, gameState):
+        max_action = Directions.STOP
+        max_q_val = 0
+        for action in gameState.getLegalActions(self.index):
+            qVal = self.getQValue(gameState, action)
+            if qVal > max_q_val or max_action is None:
+                max_q_val = qVal
+                max_action = action
+        return max_action
+    
+    def getQValue(self, state, action):
+        qVal = 0
+        features = self.getFeatures(state, action)
+        for feature in features:
+            featureValue = features[feature]
+            weight = self.weights.get(feature, 1)
+            qVal += weight * featureValue
+        return qVal
+    
+    def getFeatures(self, gameState, action):
+        features = {}
+        successor = self.getSuccessor(gameState, action)
+        features['successorScore'] = self.getScore(successor)
+
+        # Compute distance to the nearest food.
+        foodList = self.getFood(successor).asList()
+
+        # Computer distance to nearest capsule
+        capsuleList = self.getCapsules(successor)
+        oldCapsuleList = self.getCapsules(gameState)
+
+        # Get own position 
+        myPos = successor.getAgentState(self.index).getPosition()
+
+        # Add capsule as ghost
+        foodList.extend(capsuleList)
+        
+        # reward PacMan for eating a capsule 
+        if myPos in oldCapsuleList:
+            features['eatCapsule'] = 1
+
+        # This should always be True, but better safe than sorry.
+        if (len(foodList) > 0):
+            myPos = successor.getAgentState(self.index).getPosition()
+            minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+            features['distanceToFood'] = minDistance
+
+        # if capsuleList:
+        #     myPos = successor.getAgentState(self.index).getPosition()
+        #     minDistance = min([self.getMazeDistance(myPos, cap) for cap in capsuleList])
+        #     features['distanceToCapsule'] = minDistance
+
+        # if (len(foodList) > 0):
+        #     if capsuleList:
+        #         myPos = successor.getAgentState(self.index).getPosition()
+        #         minDistance_f = min([self.getMazeDistance(myPos, cap) for cap in capsuleList])
+        #         minDistance_c = min([self.getMazeDistance(myPos, food) for food in foodList])
+        #         if minDistance_f > minDistance_c:
+        #             features['distanceToFood'] = minDistance_f
+        #             features['distanceToCapsule'] = -1000000
+        #         else:
+        #             features['distanceToFood'] = -100000000000
+        #             features['distanceToCapsule'] = minDistance_c
+
+        # compute dist to enemy
+        enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
+        defenders = [a for a in enemies if a.isGhost() and a.getPosition() is not None]
+        if (len(defenders) > 0):
+            # dists = [distance.manhattan(myPos, a.getPosition()) for a in defenders]
+            dists = []
+            closestGhost = None
+            currentMin = 10000
+            for a in defenders:
+                distance = self.getMazeDistance(myPos, a.getPosition())
+                dists.append(distance)
+                # get the ghost object that is closest to Pacman
+                if distance < currentMin:
+                    currentMin = distance
+                    closestGhost = a
+            distanceToEnemy = 10
+            minDist = min(dists)
+            # if ghost is within 5 distance of pacman
+            if dists and minDist < 5 and successor.getAgentState(self.index).isPacman():
+                distanceToEnemy = minDist
+
+            # if ghosts are scared then go towards them
+            # THIS IS NOT WORKING PACMAN STILL RUNS AWAY FROM SCARED GHOSTS and doesnt eat capsules
+            if closestGhost.isScared():
+                distanceToEnemy *= -.1
+
+            features['distanceToEnemy'] = distanceToEnemy
+
+        # Discourage stopping
+        if (action == Directions.STOP):
+            features['stop'] = 1
+        
+        return features
+
+    # I think this function is called after the previous getAction() takes affect
+    # We might want to call update() here instead of chooseAction()
+    # def observationFunction(self, state):
+    #     return None
+
+    def update(self, gameState):
+        lastAction = self.lastAction
+        if not lastAction:
+            return
+        lastState = self.getPreviousObservation()
+        reward = self.getReward(gameState)
+        discount = self.discountRate
+        qPrimeVal = self.getValue(gameState)
+        qVal = self.getQValue(lastState, lastAction)
+        
+        correction = (reward + discount * qPrimeVal) - qVal
+
+        features = self.getFeatures(lastState, lastAction)
+        for feature in features:
+            newWeight = self.getWeight(feature) + self.alpha * correction * features[feature]
+            self.setWeight(feature, newWeight) 
+
+    # Calculates the reward we got for the previous action
+    def getReward(self, gameState):
+        """ ----- Notes -----
+
+        This will take some thought:
+            In P3 the reward is the change in score after we took the last action.
+            
+            We can't use this approach here because the score is affected by our opponents and the 
+            other agent on our team.
+
+            Some ideas for how to calculate reward for offensive agents:
+                - Use the change in amount of food (i.e. add a reward if we eat a food) 
+                - Add reward if we eat a power up
+            
+            I'm not sure how we will calculate rewards for invaders because there is no function 
+            to tell if we ate an invader
+
+        """
+        # TODO: rewrite this function
+        currentObv = self.getCurrentObservation()
+        prevObv = self.getPreviousObservation()
+        reward = currentObv.getScore() - prevObv.getScore()
+
+    def getWeight(self, feature):
+        if feature in self.weights:
+            return self.weights.get(feature)
+        initialWeights = {
+            'numInvaders': -1000,
+            'onDefense': 100,
+            'invaderDistance': -10,
+            'stop': -100,
+            'reverse': -2,
+            'invaderFoodDistance': 0,
+            'waitMiddle': -100
+        }
+        return initialWeights.get(feature, 1)
+
+    def setWeight(self, feature, value):
+        self.weights[feature] = value
 
